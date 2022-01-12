08e3c6b00ebc563c9cd4d3f3492b46869b23b0bc
onlyorlov <onlyorlov@gmail.com>
2022-01-12 10:32:36 +0300

    some job

--- track.py
+++ track.py
@@ -1,22 +1,4 @@
 # limit the number of cpus used by high performance libraries
-
-import argparse
-import platform
-import json
-import pickle
-import shutil
-import time
-import numpy as np
-from pathlib import Path
-import cv2
-import torch
-import torch.backends.cudnn as cudnn
-from yolov5-face.models.experimental import attempt_load
-from yolov5-face.utils.datasets import LoadImages, LoadStreams
-from yolov5-face.utils.torch_utils import select_device, time_sync
-from yolov5-face.utils.plots import Annotator, colors
-from yolov5-face.utils.general import (LOGGER, check_img_size, non_max_suppression, scale_coords,
-                                       check_imshow, xyxy2xywh, increment_path)
 import sys
 import os
 os.environ["OMP_NUM_THREADS"] = "1"
@@ -27,6 +9,23 @@ os.environ["NUMEXPR_NUM_THREADS"] = "1"
 
 sys.path.insert(0, './yolov5-face')
 
+import torch.backends.cudnn as cudnn
+import torch
+import cv2
+from pathlib import Path
+import numpy as np
+import time
+import shutil
+import pickle
+import json
+import platform
+import argparse
+
+from yolov5-face.utils.general import check_img_size, non_max_suppression_face, scale_coords, check_imshow, xyxy2xywh, increment_path
+from yolov5-face.utils.plots import Annotator, colors
+from yolov5-face.utils.torch_utils import select_device, time_sync
+from yolov5-face.utils.datasets import LoadImages, LoadStreams
+from yolov5-face.models.experimental import attempt_load
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # yolov5 deepsort root directory
@@ -34,11 +33,58 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
+def scale_coords_landmarks(img1_shape, coords, img0_shape, ratio_pad=None):
+    # Rescale coords (xyxy) from img1_shape to img0_shape
+    if ratio_pad is None:  # calculate from img0_shape
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  # gain  = old / new
+        pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+
+    coords[:, [0, 2, 4, 6, 8]] -= pad[0]  # x padding
+    coords[:, [1, 3, 5, 7, 9]] -= pad[1]  # y padding
+    coords[:, :10] /= gain
+    #clip_coords(coords, img0_shape)
+    coords[:, 0].clamp_(0, img0_shape[1])  # x1
+    coords[:, 1].clamp_(0, img0_shape[0])  # y1
+    coords[:, 2].clamp_(0, img0_shape[1])  # x2
+    coords[:, 3].clamp_(0, img0_shape[0])  # y2
+    coords[:, 4].clamp_(0, img0_shape[1])  # x3
+    coords[:, 5].clamp_(0, img0_shape[0])  # y3
+    coords[:, 6].clamp_(0, img0_shape[1])  # x4
+    coords[:, 7].clamp_(0, img0_shape[0])  # y4
+    coords[:, 8].clamp_(0, img0_shape[1])  # x5
+    coords[:, 9].clamp_(0, img0_shape[0])  # y5
+    return coords
+
+def show_results(img, xywh, conf, landmarks, class_num):
+    h,w,c = img.shape
+    tl = 1 or round(0.002 * (h + w) / 2) + 1  # line/font thickness
+    x1 = int(xywh[0] * w - 0.5 * xywh[2] * w)
+    y1 = int(xywh[1] * h - 0.5 * xywh[3] * h)
+    x2 = int(xywh[0] * w + 0.5 * xywh[2] * w)
+    y2 = int(xywh[1] * h + 0.5 * xywh[3] * h)
+    cv2.rectangle(img, (x1,y1), (x2, y2), (0,255,0), thickness=tl, lineType=cv2.LINE_AA)
+
+    clors = [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(0,255,255)]
+
+    for i in range(5):
+        point_x = int(landmarks[2 * i] * w)
+        point_y = int(landmarks[2 * i + 1] * h)
+        cv2.circle(img, (point_x, point_y), tl+1, clors[i], -1)
+
+    tf = max(tl - 1, 1)  # font thickness
+    label = str(conf)[:5]
+    cv2.putText(img, label, (x1, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+    return 
 
 def detect(opt):
-    out, source, yolo_model, show_vid, save_vid, imgsz, project, name, mask, coef, exist_ok = \
-        opt.output, opt.source, opt.yolo_model, opt.show_vid, opt.save_vid, \
-        opt.imgsz, opt.project, opt.name, opt.mask, opt.coef, opt.exist_ok
+    #проверить аргументы
+    out, source, yolo_model, save_vid, imgsz, project, name, coef, exist_ok = \
+        opt.output, opt.source, opt.yolo_model, opt.save_vid, \
+        opt.imgsz, opt.project, opt.name, opt.coef, opt.exist_ok
+    
     webcam = source == '0' or source.startswith(
         'rtsp') or source.startswith('http') or source.endswith('.txt')
 
@@ -53,25 +99,19 @@ def detect(opt):
     # Load model
     device = select_device(device)
     model = attempt_load(yolo_model, map_location=device)
-    stride, names, pt, jit, _ = model.stride, model.names, model.pt, model.jit, model.onnx
-    imgsz = check_img_size(imgsz, s=stride)  # check image size
+    imgsz = check_img_size(imgsz)  # check image size
 
     # Set Dataloader
     vid_path, vid_writer = None, None
-    # Check if environment supports image displays
-    if show_vid:
-        show_vid = check_imshow()
 
     # Dataloader
     if webcam:
         show_vid = check_imshow()
         cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz,
-                              stride=stride, mask=mask, auto=pt and not jit)
+        dataset = LoadStreams(source, img_size=imgsz)
         bs = len(dataset)  # batch_size
     else:
-        dataset = LoadImages(
-            source, img_size=imgsz, stride=stride, mask=mask, coef=coef, auto=pt and not jit)
+        dataset = LoadImages(source, img_size=imgsz, coef=coef)
         bs = 1  # batch_size
     vid_path, vid_writer = [None] * bs, [None] * bs
     # Get names and colors
@@ -79,11 +119,10 @@ def detect(opt):
 
     # extract what is in between the last '/' and last '.'
     txt_file_name = source.split('/')[-1].split('.')[0]
-    txt_path = str(Path(save_dir)) + '/' + txt_file_name + '.txt'
 
-    if pt and device.type != 'cpu':
-        model(torch.zeros(
-            1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
+    # if pt and device.type != 'cpu':
+    #     model(torch.zeros(
+    #         1, 3, *imgsz).to(device).type_as(next(model.model.parameters())))  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
 
     # output for total counter
@@ -102,65 +141,45 @@ def detect(opt):
         # Inference
         visualize = increment_path(
             save_dir / Path(path).stem, mkdir=True) if opt.visualize else False
-        pred = model(img, augment=opt.augment, visualize=visualize)
+        pred = model(img, visualize=visualize)
         t3 = time_sync()
         dt[1] += t3 - t2
 
         # Apply NMS
-        pred = non_max_suppression(
-            pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms, max_det=opt.max_det)
+        pred = non_max_suppression_face(pred, opt.conf_thres, opt.iou_thres, opt.classes)
         dt[2] += time_sync() - t3
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            if save_vid or show_vid:
-                seen += 1
-                if webcam:  # batch_size >= 1
-                    p, im0, _ = path[i], im0s[i].copy(), dataset.count
-                    s += f'{i}: '
-                else:
-                    p, im0, _ = path, im0s.copy(), getattr(dataset, 'frame', 0)
+            if webcam:  # batch_size >= 1
+                p, im0, _ = path[i], im0s[i].copy(), dataset.count
+                s += f'{i}: '
+            else:
+                p, im0, _ = path, im0s.copy(), getattr(dataset, 'frame', 0)
 
-                p = Path(p)  # to Path
-                save_path = str(save_dir / p.name)  # im.jpg, vid.mp4, ...
-                s += '%gx%g ' % img.shape[2:]  # print string
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # im.jpg, vid.mp4, ...
+            s += '%gx%g ' % img.shape[2:]  # print string
 
-                annotator = Annotator(im0, line_width=2, pil=not ascii)
-                p_count = 0
-                if det is not None and len(det):
-                    # Rescale boxes from img_size to im0 size
-                    det[:, :4] = scale_coords(
-                        img.shape[2:], det[:, :4], im0.shape).round()
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]].to(device)  # normalization gain whwh
+            gn_lks = torch.tensor(im0.shape)[[1, 0, 1, 0, 1, 0, 1, 0, 1, 0]].to(device)  # normalization gain landmarks
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
-                    # Print results
-                    for c in det[:, -1].unique():
-                        n = (det[:, -1] == c).sum()  # detections per class
-                        # add to string
-                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
-                        if int(c) == 0:
-                            p_count = n.item()
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
 
-                    xyxy = det[:, 0:4]
-                    confs = det[:, 4]
-                    clss = det[:, 5]
+                det[:, 5:15] = scale_coords_landmarks(img.shape[2:], det[:, 5:15], im0.shape).round()
 
-                    # draw boxes for visualization
-                    if len(det) > 0:
-                        for j, (bbox, cls, conf) in enumerate(zip(xyxy.cpu(), clss.cpu(), confs.cpu())):
+                for j in range(det.size()[0]):
+                    xywh = (xyxy2xywh(det[j, :4].view(1, 4)) / gn).view(-1).tolist()
+                    conf = det[j, 4].cpu().numpy()
+                    landmarks = (det[j, 5:15].view(1, 10) / gn_lks).view(-1).tolist()
+                    class_num = det[j, 15].cpu().numpy()
+                    im0 = show_results(im0, xywh, conf, landmarks, class_num)
 
-                            c = int(cls)  # integer class
-                            label = f'{names[c]} {conf:.2f}'
-                            annotator.box_label(
-                                bbox, label, color=colors(c, True))
-
-                        if mask and p_count:
-                            annotator.text(
-                                [0, 0], f'{p_count} people in target region', color=colors(0, True))
-
-                # Print time (inference-only)
-                # LOGGER.info(f'{s}Done. YOLO:({t3 - t2:.3f}s)')
-
-                im0 = annotator.result()
                 # Stream results
                 if show_vid:
                     cv2.imshow(p, im0)
@@ -185,53 +204,15 @@ def detect(opt):
                             save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer.write(im0)
 
-            else:
-                seen += 1
-                if webcam:  # batch_size >= 1
-                    p = path[i]
-                    s += f'{i}: '
-                else:
-                    p = path
-
-                p = Path(p)  # to Path
-                save_path = str(save_dir / p.name)  # im.jpg, vid.mp4, ...
-
-                p_count = 0
-                if det is not None and len(det):
-                    for c in det[:, -1].unique():
-                        n = (det[:, -1] == c).sum()  # detections per class
-                        # add to string
-                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
-                        if int(c) == 0:
-                            p_count = n.item()
-
-                # Print time (inference-only)
-                # LOGGER.info(f'{s}Done. YOLO:({t3 - t2:.3f}s)')
-
-            out.append((path, s, p_count))  # save detections
-            # Save counter
+            # New video
             if vid_path != save_path:  # if new video
-                LOGGER.info(f'{s}Done. YOLO:({t3 - t2:.3f}s)')
+                print(f'{s}Done. YOLO:({t3 - t2:.3f}s)')
                 vid_path = save_path
-                # save old video data/create initial file
-                with open(save_path+'.txt', 'wb') as fp:
-                    pickle.dump(out, fp)
-                output.extend(out)
-                out = []
 
-    # Last video
-    with open(save_path+'.txt', 'wb') as fp:
-        pickle.dump(out, fp)
-    output.extend(out)
-    out = []
-
-    # Save total results
-    with open(save_dir/'output.txt', 'wb') as fp:
-        pickle.dump(output, fp)
 
     # Print results
     t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
-    LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS \
+    print(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS \
         per image at shape {(1, 3, *imgsz)}' % t)
 
     print('Results saved to %s' % save_dir)
@@ -257,31 +238,14 @@ if __name__ == '__main__':
                         help='output video codec (verify ffmpeg support)')
     parser.add_argument('--device', default='',
                         help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--show-vid', action='store_true',
-                        help='display tracking video results')
     parser.add_argument('--save-vid', action='store_true',
                         help='save video tracking results')
-    # class 0 is person, 1 is bycicle, 2 is car... 79 is oven
-    parser.add_argument('--classes', nargs='+', type=int,
-                        help='filter by class: --class 0, or --class 16 17')
-    parser.add_argument('--agnostic-nms', action='store_true',
-                        help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true',
-                        help='augmented inference')
-    parser.add_argument('--evaluate', action='store_true',
-                        help='augmented inference')
-    parser.add_argument('--visualize', action='store_true',
-                        help='visualize features')
     parser.add_argument('--max-det', type=int, default=1000,
                         help='maximum detection per image')
-    parser.add_argument('--dnn', action='store_true',
-                        help='use OpenCV DNN for ONNX inference')
     parser.add_argument('--project', default=ROOT / 'runs',
                         help='save results to project/name')
     parser.add_argument('--name', default='exp',
                         help='save results to project/name')
-    parser.add_argument('--mask', type=json.loads, default=None,
-                        help='all outer corners of the region of interest(ROI)')
     parser.add_argument('--coef', type=int, default=1,
                         help='desired coef to decrease fps')
     parser.add_argument('--exist-ok', action='store_true',
